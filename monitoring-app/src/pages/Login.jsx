27c2f78df2989090778fc90ab0d2aa5516a59cc3
Lee Anne Angeles <leeangeles2@gmail.com>
2024-12-08 22:34:35 +0800

    feat: enhance styles of login and register page

--- monitoring-app/src/pages/Login.jsx
+++ monitoring-app/src/pages/Login.jsx
@@ -24,17 +24,17 @@ const Login = ({ onLogin }) => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-[#020024] via-[#090979] to-[#00d4ff]" >
-      {/* Glassmorphism Container */}
-      <div className="w-full max-w-sm p-10 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg shadow-lg">
-        <h2 className="text-3xl font-bold text-center mb-6 text-white">
+    <div className="flex items-center justify-center min-h-screen bg-[linear-gradient(to_bottom_right,_#020024_0%,_#090979_50%,_#0648a3_75%,_#00d4ff_100%)]">
+    {/* Glassmorphism Container */}
+      <div className="w-full max-w-md p-10 bg-black bg-opacity-20 backdrop-blur-lg rounded-lg shadow-lg">
+        <h2 className="text-3xl font-bold text-center mb-6 text-teal-400">
           Login
         </h2>
         <form onSubmit={handleSubmit} className="space-y-6">
           {/* Email Field */}
           <div className="relative">
-            <label htmlFor="email" className="block text-sm font-medium text-white">
-              Your Email
+            <label htmlFor="email" className="block text-base font-medium text-teal-400">
+              Email
             </label>
             <div className="flex items-center mt-2">
               <span className="absolute right-4 text-gray-300">
@@ -43,7 +43,7 @@ const Login = ({ onLogin }) => {
               <input
                 type="email"
                 id="email"
-                className="w-full px-4 pl-4 py-3 bg-white bg-opacity-20 text-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-white placeholder-opacity-50"
+                className="w-full px-4 pl-4 py-3 bg-gray-600 bg-opacity-20 text-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-white placeholder-opacity-50 tracking-wide"
                 placeholder="Enter your email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -54,8 +54,8 @@ const Login = ({ onLogin }) => {
 
           {/* Password Field */}
           <div className="relative">
-            <label htmlFor="password" className="block text-sm font-medium text-white">
-              Your Password
+            <label htmlFor="password" className="block text-base font-medium text-teal-400">
+              Password
             </label>
             <div className="flex items-center mt-2">
               <span
@@ -67,7 +67,7 @@ const Login = ({ onLogin }) => {
               <input
                 type={showPassword ? "text" : "password"}
                 id="password"
-                className="w-full px-4 pl-4 py-3 bg-white bg-opacity-20 text-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-white placeholder-opacity-50"
+                className="w-full px-4 pl-4 py-3 bg-gray-600 bg-opacity-20 text-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-white placeholder-opacity-50 tracking-wide"
                 placeholder="Enter your password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -79,17 +79,17 @@ const Login = ({ onLogin }) => {
           {/* Submit Button */}
           <button
             type="submit"
-            className="w-full py-3 mt-4 bg-gradient-to-r from-blue-500 to-indigo-500 text-white text-lg font-medium rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 transition-all tracking-wide"
+            className="w-full py-3 mt-4 bg-teal-400 text-white text-xl font-medium rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 transition-all tracking-wide"
           >
             Login
           </button>
         </form>
 
         {/* Register Link */}
-        <p className="mt-6 text-sm text-center text-white">
+        <p className="mt-6 text-md text-center text-white tracking-wide">
           Already have an account?{" "}
           <button
-            className="text-blue-400 hover:underline"
+            className="text-teal-500 hover:underline"
             onClick={() => navigate("/register")}
           >
             Register
--- monitoring-app/src/pages/Register.jsx
+++ monitoring-app/src/pages/Register.jsx
@@ -31,15 +31,15 @@ const Register = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-[#020024] via-[#090979] to-[#00d4ff]">
+    <div className="flex items-center justify-center min-h-screen bg-[linear-gradient(to_bottom_right,_#020024_0%,_#090979_50%,_#0648a3_75%,_#00d4ff_100%)]">
       {/* Glassmorphism Container */}
-      <div className="w-full max-w-sm p-10 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg shadow-lg">
-        <h2 className="text-3xl font-bold text-center mb-6 text-white">Register</h2>
+      <div className="w-full max-w-md p-10 bg-black bg-opacity-20 backdrop-blur-lg rounded-lg shadow-lg">
+        <h2 className="text-3xl font-bold text-center mb-6 text-teal-400">Register</h2>
         <form onSubmit={handleRegister} className="space-y-6">
           {/* Email Field */}
           <div className="relative">
-            <label htmlFor="email" className="block text-sm font-medium text-white">
-              Your Email
+            <label htmlFor="email" className="block text-base font-semibold text-teal-400">
+              Email
             </label>
             <div className="flex items-center mt-2">
               <span className="absolute right-4 text-gray-300">
@@ -48,7 +48,7 @@ const Register = () => {
               <input
                 type="email"
                 id="email"
-                className="w-full px-4 pl-4 py-3 bg-white bg-opacity-20 text-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-white placeholder-opacity-50"
+                className="w-full px-4 pl-4 py-3 bg-gray-600 bg-opacity-20 text-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-white placeholder-opacity-50"
                 placeholder="Enter your email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -59,8 +59,8 @@ const Register = () => {
 
           {/* Password Field */}
           <div className="relative">
-            <label htmlFor="password" className="block text-sm font-medium text-white">
-              Your Password
+            <label htmlFor="password" className="block text-base font-semibold text-teal-400">
+              Password
             </label>
             <div className="flex items-center mt-2">
               <span
@@ -72,7 +72,7 @@ const Register = () => {
               <input
                 type={showPassword ? "text" : "password"}
                 id="password"
-                className="w-full px-4 pl-4 py-3 bg-white bg-opacity-20 text-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-white placeholder-opacity-50"
+                className="w-full px-4 pl-4 py-3 bg-gray-600 bg-opacity-20 text-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-white placeholder-opacity-50"
                 placeholder="Enter your password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -83,7 +83,7 @@ const Register = () => {
 
           {/* Confirm Password Field */}
           <div className="relative">
-            <label htmlFor="confirmPassword" className="block text-sm font-medium text-white">
+            <label htmlFor="confirmPassword" className="block text-base font-semibold text-teal-400">
               Confirm Password
             </label>
             <div className="flex items-center mt-2">
@@ -96,8 +96,8 @@ const Register = () => {
               <input
                 type={showConfirmPassword ? "text" : "password"}
                 id="confirmPassword"
-                className="w-full px-4 pl-4 py-3 bg-white bg-opacity-20 text-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-white placeholder-opacity-50"
-                placeholder="Confirm your password"
+                className="w-full px-4 pl-4 py-3 bg-gray-600 bg-opacity-20 text-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-white placeholder-opacity-50"
+                placeholder="Re-enter your password"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 required
@@ -108,17 +108,17 @@ const Register = () => {
           {/* Submit Button */}
           <button
             type="submit"
-            className="w-full py-3 mt-4 bg-gradient-to-r from-blue-500 to-indigo-500 text-white text-lg font-medium rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 transition-all tracking-wide"
+            className="w-full py-3 mt-4 bg-teal-500 text-white text-xl font-medium rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 transition-all tracking-wide"
           >
             Register
           </button>
         </form>
 
         {/* Login Link */}
-        <p className="mt-6 text-sm text-center text-white">
+        <p className="mt-6 text-base text-center text-white tracking-wide">
           Already have an account?{" "}
           <button
-            className="text-blue-400 hover:underline"
+            className="text-[#00d4ff] hover:underline"
             onClick={() => navigate("/login")}
           >
             Login
