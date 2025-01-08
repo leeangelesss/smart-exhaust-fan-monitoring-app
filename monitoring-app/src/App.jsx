1c37b3024f08cf8b6011699fd894ea469a56fd52
JosephBaturiano <josephbaturiano3@gmail.com>
2025-01-08 23:42:25 +0800

    fix: import profile from app

--- monitoring-app/src/App.jsx
+++ monitoring-app/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import Profile from "./pages/Profile";
+import Profile from "./pages/profile";
 import { supabase } from "./utils/supabase"; // Ensure correct path
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
