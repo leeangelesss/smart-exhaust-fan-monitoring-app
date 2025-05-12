9dd3cc66c176b555a98c9c160804af25baeb6132
leeangelesss <leeangeles2@gmail.com>
2025-05-12 13:40:41 +0800

    feat: enhace UI of legend

--- monitoring-app/src/components/Legend.jsx
+++ monitoring-app/src/components/Legend.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 
 const Legend = () => (
-  <div className="flex flex-wrap items-center justify-center bg-black p-4 rounded-lg text-white w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl mx-auto bg-opacity-25">
-    <div className="flex flex-col sm:flex-row items-center mx-2 my-2">
-      <div className="flex items-center mx-2 my-2">
-        <span className="bg-green-500 w-4 h-4 sm:w-6 sm:h-6 rounded-full inline-block mr-2"></span> 
+  <div className="overflow-x-auto">
+    <div className="flex items-center justify-center bg-black bg-opacity-25 py-6 px-8 rounded-lg text-white w-full max-w-full space-x-6 sm:space-x-8 mx-auto">
+      <div className="flex items-center space-x-2">
+        <span className="bg-green-500 w-4 h-4 sm:w-6 sm:h-6 rounded-full inline-block"></span>
         <span className="text-sm sm:text-base">Good</span>
       </div>
-      <div className="flex items-center mx-2 my-2">
-        <span className="bg-yellow-500 w-4 h-4 sm:w-6 sm:h-6 rounded-full inline-block mr-2"></span> 
+      <div className="flex items-center space-x-2">
+        <span className="bg-yellow-500 w-4 h-4 sm:w-6 sm:h-6 rounded-full inline-block"></span>
         <span className="text-sm sm:text-base">Caution</span>
       </div>
-    </div>
-    <div className="flex flex-col sm:flex-row items-center mx-2 my-2">
-      <div className="flex items-center mx-2 my-2">
-        <span className="bg-orange-500 w-4 h-4 sm:w-6 sm:h-6 rounded-full inline-block mr-2"></span> 
+      <div className="flex items-center space-x-2">
+        <span className="bg-orange-500 w-4 h-4 sm:w-6 sm:h-6 rounded-full inline-block"></span>
         <span className="text-sm sm:text-base">Danger</span>
       </div>
-      <div className="flex items-center mx-2 my-2">
-        <span className="bg-red-800 w-4 h-4 sm:w-6 sm:h-6 rounded-full inline-block mr-2"></span> 
+      <div className="flex items-center space-x-2">
+        <span className="bg-red-800 w-4 h-4 sm:w-6 sm:h-6 rounded-full inline-block"></span>
         <span className="text-sm sm:text-base">Critical</span>
       </div>
     </div>
